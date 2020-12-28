210840937c145eb957b0adaa65426fb3388398e5
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2020-12-28 17:07:47 +0100

    Commit general

---
+++ pokemon.js
@@ -0,0 +1,9 @@
+//let i = True;
+//if (i) {
+let a = prompt("Salut jeune dresseur! Comment t'appelles tu?");
+document.write(`Bienvenue, ${a}`);
+let b = prompt("Aime tu Pokemon? (oui/non)");
+if (b === "oui") alert("c'est bien");
+else if (b === "non") alert("c'est dommage :(");
+else alert("Expression non valide");
+//}
